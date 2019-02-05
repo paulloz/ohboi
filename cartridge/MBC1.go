04bb2a0b53b6c89bed45121ecca864eb4f089f18
Paul Joannon <hello@pauljoannon.com>
2019-02-05 10:19:08 +0100

    MBC1 ROM banking is fully functionnal

--- cartridge/MBC1.go
+++ cartridge/MBC1.go
@@ -5,11 +5,13 @@ import (
 )
 
 type MBC1 struct {
-	rom     []Byte
-	romBank Byte
+	rom []Byte
 
-	isRAMBanking bool
-	isRAMEnabled bool
+	romBank            Byte
+	activeRomBankStart Word
+
+	isRAMBanking       bool
+	isRAMEnabled       bool
 }
 
 func NewMBC1(rom []Byte) *MBC1 {
@@ -23,24 +25,34 @@ func NewMBC1(rom []Byte) *MBC1 {
 }
 
 func (c *MBC1) Read(address Word) Byte {
-	if address > 0x3FFF {
-		return 0
+	if address <= 0x3FFF {
+		return c.rom[address] // First bank is always there
+	} else if address <= 0x7FFF {
+		return c.rom[address-0x4000+c.activeRomBankStart]
 	}
 
-	return c.rom[address] // First bank is always there
+	return 0
 }
 
 func (c *MBC1) Write(address Word, data Byte) {
 	if address <= 0x1FFF { // 0x0A on lower 4 bits enable RAM, other values disable RAM
 		c.isRAMEnabled = (data & 0xF) == 0xA
 	} else if address <= 0x3FFF { // Lower 5bits of romBank
-		c.romBank = (c.romBank & 0x60) | (data & 0x1F)
+		c.bankROM((c.romBank & 0x60) | (data & 0x1F))
 	} else if address <= 0x5FFF {
 		if c.isRAMBanking {
 		} else { // Bits 5 and 6 of romBank
-			c.romBank = (c.romBank & 0x1F) | (data & 0x60)
+			c.bankROM((c.romBank & 0x1F) | (data & 0x60))
 		}
 	} else if address <= 0x6FFF {
 		c.isRAMBanking = (data & 1) != 0
 	}
+}
+
+func (c *MBC1) bankROM(newBank Byte) {
+	if newBank == 0x00 || newBank == 0x20 || newBank == 0x40 || newBank == 0x60 {
+		newBank += 1
+	}
+	c.romBank = newBank
+	c.activeRomBankStart = Word(c.romBank) * Word(0x4000)
 }
