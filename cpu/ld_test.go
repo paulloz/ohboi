c905ad902b1083cd1967a5ba985f9d17ff107ce3
Sylvain Baubeau <lebauce@gmail.com>
2019-02-07 01:59:20 +0100

    Move push and pop tests to the proper place

--- cpu/ld_test.go
+++ cpu/ld_test.go
@@ -265,33 +265,3 @@ func TestOpcodeLD_NN_SP(t *testing.T) {
 		t.Errorf("Expected address 0xff80 to contain 0xabcd, got %x", cpu.mem.ReadWord(0x80, 0xff))
 	}
 }
-
-func TestOpcodePUSH_AF(t *testing.T) {
-	cpu := newTestCPU([]byte{op.PUSH_AF})
-	cpu.SP.Set(0xff80)
-	cpu.AF.Set(123)
-
-	_, err := cpu.ExecuteOpCode()
-	if err != nil {
-		t.Error(err)
-	}
-
-	if cpu.mem.ReadWord(0x80, 0xff) != 123 {
-		t.Errorf("Expected address 0xff80 to contain 123, got %x", cpu.mem.ReadWord(0x80, 0xff))
-	}
-}
-
-func TestOpcodePOP_AF(t *testing.T) {
-	cpu := newTestCPU([]byte{op.POP_AF})
-	cpu.mem.WriteWord(0x80, 0xff, 0xabcd)
-	cpu.SP.Set(0xff80)
-
-	_, err := cpu.ExecuteOpCode()
-	if err != nil {
-		t.Error(err)
-	}
-
-	if cpu.AF.Get() != 0xabcd {
-		t.Errorf("Expected AF to contain 0xabcd, got %x", cpu.AF.Get())
-	}
-}
