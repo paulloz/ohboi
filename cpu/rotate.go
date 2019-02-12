8e10cf2df32635d914894af7dc198165e05fad3b
Paul Joannon <hello@pauljoannon.com>
2019-02-12 12:04:28 +0100

    fix shifts and rotates

--- cpu/rotate.go
+++ cpu/rotate.go
@@ -6,21 +6,18 @@ import (
 	"github.com/paulloz/ohboi/memory"
 )
 
-func newRL(getset GetterSetter, quick bool) Instruction {
-	cycles := uint32(8)
-	if getset == AddressHL {
-		cycles = 16
-	}
-	if quick {
-		cycles /= 2
-	}
+func rl(v uint8, c bool) uint8 {
+	return (v << 1) | bits.FromBool(c)
+}
 
+func newRL(getset GetterSetter, cycles uint32) Instruction {
 	return Instruction{
 		Handler: func(cpu *CPU, mem *memory.Memory) error {
 			in := getset.Get(cpu)
-			out := (in << 1) | bits.FromBool(cpu.GetCFlag())
 
+			out := rl(in, cpu.GetCFlag())
 			getset.Set(cpu, out)
+
 			cpu.SetZFlag(out == 0)
 			cpu.SetNFlag(false)
 			cpu.SetHFlag(false)
@@ -32,21 +29,18 @@ func newRL(getset GetterSetter, quick bool) Instruction {
 	}
 }
 
-func newRR(getset GetterSetter, quick bool) Instruction {
-	cycles := uint32(8)
-	if getset == AddressHL {
-		cycles = 16
-	}
-	if quick {
-		cycles /= 2
-	}
+func rr(v uint8, c bool) uint8 {
+	return (bits.FromBool(c) << 7) | (v >> 1)
+}
 
+func newRR(getset GetterSetter, cycles uint32) Instruction {
 	return Instruction{
 		Handler: func(cpu *CPU, mem *memory.Memory) error {
 			in := getset.Get(cpu)
-			out := (in >> 1) | (bits.FromBool(cpu.GetCFlag()) << 7)
 
+			out := rr(in, cpu.GetCFlag())
 			getset.Set(cpu, out)
+
 			cpu.SetZFlag(out == 0)
 			cpu.SetNFlag(false)
 			cpu.SetHFlag(false)
@@ -58,26 +52,22 @@ func newRR(getset GetterSetter, quick bool) Instruction {
 	}
 }
 
-func newRLC(getset GetterSetter, quick bool) Instruction {
-	cycles := uint32(8)
-	if getset == AddressHL {
-		cycles = 16
-	}
-	if quick {
-		cycles /= 2
-	}
+func rlc(v uint8) uint8 {
+	return (v << 1) | (v >> 7)
+}
 
+func newRLC(getset GetterSetter, cycles uint32) Instruction {
 	return Instruction{
 		Handler: func(cpu *CPU, mem *memory.Memory) error {
 			in := getset.Get(cpu)
-			oldHighest := in >> 7
-			out := (in << 1) | oldHighest
 
+			out := rlc(in)
 			getset.Set(cpu, out)
+
 			cpu.SetZFlag(out == 0)
 			cpu.SetNFlag(false)
 			cpu.SetHFlag(false)
-			cpu.SetCFlag(oldHighest == 1)
+			cpu.SetCFlag(in > 0x7f)
 
 			return nil
 		},
@@ -85,26 +75,22 @@ func newRLC(getset GetterSetter, quick bool) Instruction {
 	}
 }
 
-func newRRC(getset GetterSetter, quick bool) Instruction {
-	cycles := uint32(8)
-	if getset == AddressHL {
-		cycles = 16
-	}
-	if quick {
-		cycles /= 2
-	}
+func rrc(v uint8) uint8 {
+	return ((v & 0x01) << 7) | (v >> 1)
+}
 
+func newRRC(getset GetterSetter, cycles uint32) Instruction {
 	return Instruction{
 		Handler: func(cpu *CPU, mem *memory.Memory) error {
 			in := getset.Get(cpu)
-			oldLowest := in & 1
-			out := (in >> 1) | (oldLowest << 7)
 
+			out := rrc(in)
 			getset.Set(cpu, out)
+
 			cpu.SetZFlag(out == 0)
 			cpu.SetNFlag(false)
 			cpu.SetHFlag(false)
-			cpu.SetCFlag(oldLowest == 1)
+			cpu.SetCFlag((in & 0x01) == 1)
 
 			return nil
 		},
@@ -112,21 +98,14 @@ func newRRC(getset GetterSetter, quick bool) Instruction {
 	}
 }
 
-func newSLA(getset GetterSetter, quick bool) Instruction {
-	cycles := uint32(8)
-	if getset == AddressHL {
-		cycles = 16
-	}
-	if quick {
-		cycles /= 2
-	}
-
+func newSLA(getset GetterSetter, cycles uint32) Instruction {
 	return Instruction{
 		Handler: func(cpu *CPU, mem *memory.Memory) error {
 			in := getset.Get(cpu)
+
 			out := in << 1
-
 			getset.Set(cpu, out)
+
 			cpu.SetZFlag(out == 0)
 			cpu.SetNFlag(false)
 			cpu.SetHFlag(false)
@@ -138,25 +117,18 @@ func newSLA(getset GetterSetter, quick bool) Instruction {
 	}
 }
 
-func newSRA(getset GetterSetter, quick bool) Instruction {
-	cycles := uint32(8)
-	if getset == AddressHL {
-		cycles = 16
-	}
-	if quick {
-		cycles /= 2
-	}
-
+func newSRA(getset GetterSetter, cycles uint32) Instruction {
 	return Instruction{
 		Handler: func(cpu *CPU, mem *memory.Memory) error {
 			in := getset.Get(cpu)
-			out := in>>1 | in&0x80
 
+			out := (in >> 1) | (in & 0x80)
 			getset.Set(cpu, out)
+
 			cpu.SetZFlag(out == 0)
 			cpu.SetNFlag(false)
 			cpu.SetHFlag(false)
-			cpu.SetCFlag(in&1 != 0)
+			cpu.SetCFlag(bits.Test(0, in))
 
 			return nil
 		},
@@ -164,25 +136,18 @@ func newSRA(getset GetterSetter, quick bool) Instruction {
 	}
 }
 
-func newSRL(getset GetterSetter, quick bool) Instruction {
-	cycles := uint32(8)
-	if getset == AddressHL {
-		cycles = 16
-	}
-	if quick {
-		cycles /= 2
-	}
-
+func newSRL(getset GetterSetter, cycles uint32) Instruction {
 	return Instruction{
 		Handler: func(cpu *CPU, mem *memory.Memory) error {
 			in := getset.Get(cpu)
-			out := in >> 1
 
+			out := in >> 1
 			getset.Set(cpu, out)
+
 			cpu.SetZFlag(out == 0)
 			cpu.SetNFlag(false)
 			cpu.SetHFlag(false)
-			cpu.SetCFlag(in&1 != 0)
+			cpu.SetCFlag(bits.Test(0, in))
 
 			return nil
 		},
@@ -192,73 +157,129 @@ func newSRL(getset GetterSetter, quick bool) Instruction {
 
 func init() {
 	RegisterInstructions(map[uint8]Instruction{
-		op.RLA:  newRL(RegisterA, true),
-		op.RRA:  newRR(RegisterA, true),
-		op.RLCA: newRLC(RegisterA, true),
-		op.RRCA: newRRC(RegisterA, true),
+		op.RLA: Instruction{
+			Handler: func(cpu *CPU, mem *memory.Memory) error {
+				in := cpu.A.Get()
+
+				cpu.A.Set(rl(in, cpu.GetCFlag()))
+
+				cpu.SetZFlag(false)
+				cpu.SetNFlag(false)
+				cpu.SetHFlag(false)
+				cpu.SetCFlag(bits.Test(7, in))
+
+				return nil
+			},
+			Cycles: 4,
+		},
+		op.RRA: Instruction{
+			Handler: func(cpu *CPU, mem *memory.Memory) error {
+				in := cpu.A.Get()
+
+				cpu.A.Set(rr(in, cpu.GetCFlag()))
+
+				cpu.SetZFlag(false)
+				cpu.SetNFlag(false)
+				cpu.SetHFlag(false)
+				cpu.SetCFlag(in&1 != 0)
+
+				return nil
+			},
+			Cycles: 4,
+		},
+		op.RLCA: Instruction{
+			Handler: func(cpu *CPU, mem *memory.Memory) error {
+				in := cpu.A.Get()
+
+				cpu.A.Set(rlc(in))
+
+				cpu.SetZFlag(false)
+				cpu.SetNFlag(false)
+				cpu.SetHFlag(false)
+				cpu.SetCFlag(in > 0x7f)
+
+				return nil
+			},
+			Cycles: 4,
+		},
+		op.RRCA: Instruction{
+			Handler: func(cpu *CPU, mem *memory.Memory) error {
+				in := cpu.A.Get()
+
+				cpu.A.Set(rrc(in))
+
+				cpu.SetZFlag(false)
+				cpu.SetNFlag(false)
+				cpu.SetHFlag(false)
+				cpu.SetCFlag((in & 0x01) == 1)
+
+				return nil
+			},
+			Cycles: 4,
+		},
 	})
 	RegisterExtInstructions(map[uint8]Instruction{
-		op.RL_A:  newRL(RegisterA, false),
-		op.RL_B:  newRL(RegisterB, false),
-		op.RL_C:  newRL(RegisterC, false),
-		op.RL_D:  newRL(RegisterD, false),
-		op.RL_E:  newRL(RegisterE, false),
-		op.RL_H:  newRL(RegisterH, false),
-		op.RL_L:  newRL(RegisterL, false),
-		op.RL_HL: newRL(AddressHL, false),
+		op.RL_A:  newRL(RegisterA, 8),
+		op.RL_B:  newRL(RegisterB, 8),
+		op.RL_C:  newRL(RegisterC, 8),
+		op.RL_D:  newRL(RegisterD, 8),
+		op.RL_E:  newRL(RegisterE, 8),
+		op.RL_H:  newRL(RegisterH, 8),
+		op.RL_L:  newRL(RegisterL, 8),
+		op.RL_HL: newRL(AddressHL, 16),
 
-		op.RR_A:  newRR(RegisterA, false),
-		op.RR_B:  newRR(RegisterB, false),
-		op.RR_C:  newRR(RegisterC, false),
-		op.RR_D:  newRR(RegisterD, false),
-		op.RR_E:  newRR(RegisterE, false),
-		op.RR_H:  newRR(RegisterH, false),
-		op.RR_L:  newRR(RegisterL, false),
-		op.RR_HL: newRR(AddressHL, false),
+		op.RR_A:  newRR(RegisterA, 8),
+		op.RR_B:  newRR(RegisterB, 8),
+		op.RR_C:  newRR(RegisterC, 8),
+		op.RR_D:  newRR(RegisterD, 8),
+		op.RR_E:  newRR(RegisterE, 8),
+		op.RR_H:  newRR(RegisterH, 8),
+		op.RR_L:  newRR(RegisterL, 8),
+		op.RR_HL: newRR(AddressHL, 16),
 
-		op.RLC_A:  newRLC(RegisterA, false),
-		op.RLC_B:  newRLC(RegisterB, false),
-		op.RLC_C:  newRLC(RegisterC, false),
-		op.RLC_D:  newRLC(RegisterD, false),
-		op.RLC_E:  newRLC(RegisterE, false),
-		op.RLC_H:  newRLC(RegisterH, false),
-		op.RLC_L:  newRLC(RegisterL, false),
-		op.RLC_HL: newRLC(AddressHL, false),
+		op.RLC_A:  newRLC(RegisterA, 8),
+		op.RLC_B:  newRLC(RegisterB, 8),
+		op.RLC_C:  newRLC(RegisterC, 8),
+		op.RLC_D:  newRLC(RegisterD, 8),
+		op.RLC_E:  newRLC(RegisterE, 8),
+		op.RLC_H:  newRLC(RegisterH, 8),
+		op.RLC_L:  newRLC(RegisterL, 8),
+		op.RLC_HL: newRLC(AddressHL, 16),
 
-		op.RRC_A:  newRRC(RegisterA, false),
-		op.RRC_B:  newRRC(RegisterB, false),
-		op.RRC_C:  newRRC(RegisterC, false),
-		op.RRC_D:  newRRC(RegisterD, false),
-		op.RRC_E:  newRRC(RegisterE, false),
-		op.RRC_H:  newRRC(RegisterH, false),
-		op.RRC_L:  newRRC(RegisterL, false),
-		op.RRC_HL: newRRC(AddressHL, false),
+		op.RRC_A:  newRRC(RegisterA, 8),
+		op.RRC_B:  newRRC(RegisterB, 8),
+		op.RRC_C:  newRRC(RegisterC, 8),
+		op.RRC_D:  newRRC(RegisterD, 8),
+		op.RRC_E:  newRRC(RegisterE, 8),
+		op.RRC_H:  newRRC(RegisterH, 8),
+		op.RRC_L:  newRRC(RegisterL, 8),
+		op.RRC_HL: newRRC(AddressHL, 16),
 
-		op.SLA_A:  newSLA(RegisterA, false),
-		op.SLA_B:  newSLA(RegisterB, false),
-		op.SLA_C:  newSLA(RegisterC, false),
-		op.SLA_D:  newSLA(RegisterD, false),
-		op.SLA_E:  newSLA(RegisterE, false),
-		op.SLA_H:  newSLA(RegisterH, false),
-		op.SLA_L:  newSLA(RegisterL, false),
-		op.SLA_HL: newSLA(AddressHL, false),
+		op.SLA_A:  newSLA(RegisterA, 8),
+		op.SLA_B:  newSLA(RegisterB, 8),
+		op.SLA_C:  newSLA(RegisterC, 8),
+		op.SLA_D:  newSLA(RegisterD, 8),
+		op.SLA_E:  newSLA(RegisterE, 8),
+		op.SLA_H:  newSLA(RegisterH, 8),
+		op.SLA_L:  newSLA(RegisterL, 8),
+		op.SLA_HL: newSLA(AddressHL, 16),
 
-		op.SRA_A:  newSRA(RegisterA, false),
-		op.SRA_B:  newSRA(RegisterB, false),
-		op.SRA_C:  newSRA(RegisterC, false),
-		op.SRA_D:  newSRA(RegisterD, false),
-		op.SRA_E:  newSRA(RegisterE, false),
-		op.SRA_H:  newSRA(RegisterH, false),
-		op.SRA_L:  newSRA(RegisterL, false),
-		op.SRA_HL: newSRA(AddressHL, false),
+		op.SRA_A:  newSRA(RegisterA, 8),
+		op.SRA_B:  newSRA(RegisterB, 8),
+		op.SRA_C:  newSRA(RegisterC, 8),
+		op.SRA_D:  newSRA(RegisterD, 8),
+		op.SRA_E:  newSRA(RegisterE, 8),
+		op.SRA_H:  newSRA(RegisterH, 8),
+		op.SRA_L:  newSRA(RegisterL, 8),
+		op.SRA_HL: newSRA(AddressHL, 16),
 
-		op.SRL_A:  newSRL(RegisterA, false),
-		op.SRL_B:  newSRL(RegisterB, false),
-		op.SRL_C:  newSRL(RegisterC, false),
-		op.SRL_D:  newSRL(RegisterD, false),
-		op.SRL_E:  newSRL(RegisterE, false),
-		op.SRL_H:  newSRL(RegisterH, false),
-		op.SRL_L:  newSRL(RegisterL, false),
-		op.SRL_HL: newSRL(AddressHL, false),
+		op.SRL_A:  newSRL(RegisterA, 8),
+		op.SRL_B:  newSRL(RegisterB, 8),
+		op.SRL_C:  newSRL(RegisterC, 8),
+		op.SRL_D:  newSRL(RegisterD, 8),
+		op.SRL_E:  newSRL(RegisterE, 8),
+		op.SRL_H:  newSRL(RegisterH, 8),
+		op.SRL_L:  newSRL(RegisterL, 8),
+		op.SRL_HL: newSRL(AddressHL, 16),
 	})
 }
