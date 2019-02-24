348ef8f571b73b58df3b3b3048152dddeb8f4ed9
Sylvain Baubeau <lebauce@gmail.com>
2019-02-24 13:19:54 +0100

    Check ROM length when reading

--- cartridge/ROM.go
+++ cartridge/ROM.go
@@ -6,7 +6,10 @@ type ROM struct {
 }
 
 func (c *ROM) Read(address uint16) uint8 {
-	return c.rom[address]
+	if address < uint16(len(c.rom)) {
+		return c.rom[address]
+	}
+	return 0xff
 }
 
 func (c *ROM) Write(address uint16, value uint8) {}
