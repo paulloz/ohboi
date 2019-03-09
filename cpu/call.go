5bc77500f6d60fb5c95837f4fadb2601fc56121a
Sylvain Baubeau <lebauce@gmail.com>
2019-03-09 11:34:34 +0100

    Fix CALL timing

--- cpu/call.go
+++ cpu/call.go
@@ -5,63 +5,44 @@ import (
 	"github.com/paulloz/ohboi/memory"
 )
 
-func (cpu *CPU) Call(nn uint16) {
-	cpu.Push(cpu.PC)
-	cpu.PC = nn
+func newCallNN(condition func(cpu *CPU, mem *memory.Memory) bool) Instruction {
+	return NewInstruction(
+		DecodeInstruction,
+		FetchByte(&AddressImmediateOperand.lo),
+		FetchByte(&AddressImmediateOperand.hi).Combine(SkipIf(condition)),
+		NoopInstruction,
+		func(cpu *CPU, mem *memory.Memory) error {
+			cpu.PushByte(uint8(cpu.PC >> 8))
+			return nil
+		},
+		func(cpu *CPU, mem *memory.Memory) error {
+			cpu.PushByte(uint8(cpu.PC))
+			cpu.PC = AddressImmediateOperand.Address()
+			return nil
+		},
+	)
 }
 
 func init() {
 	RegisterInstructions(map[uint8]Instruction{
-		op.CALL_NN: {
-			Handler: func(cpu *CPU, mem *memory.Memory) error {
-				cpu.Call(cpu.FetchWord())
-				return nil
-			},
-			Cycles: 12,
-		},
+		op.CALL_NN: newCallNN(func(cpu *CPU, mem *memory.Memory) bool {
+			return true
+		}),
 
-		op.CALL_C_NN: {
-			Handler: func(cpu *CPU, mem *memory.Memory) error {
-				addr := cpu.FetchWord()
-				if cpu.GetCFlag() {
-					cpu.Call(addr)
-				}
-				return nil
-			},
-			Cycles: 12,
-		},
+		op.CALL_C_NN: newCallNN(func(cpu *CPU, mem *memory.Memory) bool {
+			return cpu.GetCFlag()
+		}),
 
-		op.CALL_NC_NN: {
-			Handler: func(cpu *CPU, mem *memory.Memory) error {
-				addr := cpu.FetchWord()
-				if !cpu.GetCFlag() {
-					cpu.Call(addr)
-				}
-				return nil
-			},
-			Cycles: 12,
-		},
+		op.CALL_NC_NN: newCallNN(func(cpu *CPU, mem *memory.Memory) bool {
+			return !cpu.GetCFlag()
+		}),
 
-		op.CALL_Z_NN: {
-			Handler: func(cpu *CPU, mem *memory.Memory) error {
-				addr := cpu.FetchWord()
-				if cpu.GetZFlag() {
-					cpu.Call(addr)
-				}
-				return nil
-			},
-			Cycles: 12,
-		},
+		op.CALL_Z_NN: newCallNN(func(cpu *CPU, mem *memory.Memory) bool {
+			return cpu.GetZFlag()
+		}),
 
-		op.CALL_NZ_NN: {
-			Handler: func(cpu *CPU, mem *memory.Memory) error {
-				addr := cpu.FetchWord()
-				if !cpu.GetZFlag() {
-					cpu.Call(addr)
-				}
-				return nil
-			},
-			Cycles: 12,
-		},
+		op.CALL_NZ_NN: newCallNN(func(cpu *CPU, mem *memory.Memory) bool {
+			return !cpu.GetZFlag()
+		}),
 	})
 }
